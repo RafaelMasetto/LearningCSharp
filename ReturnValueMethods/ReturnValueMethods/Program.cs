112482b162e55488a8178927024112ac80c3529c
RafaelMasetto <127772302+RafaelMasetto@users.noreply.github.com>
2024-01-15 07:55:15 -0300

    Add files via upload

---
+++ ReturnValueMethods/ReturnValueMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ReturnValueMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(15, 31));
+            Console.WriteLine(Multiply(4, 5));
+            Console.WriteLine(Divide(15, 7));
+            Console.Read();
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double Divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    }
+}

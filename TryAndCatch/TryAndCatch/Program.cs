d52633a51f3b79b82d94df83e26311bd0b0a89a6
RafaelMasetto <127772302+RafaelMasetto@users.noreply.github.com>
2024-01-15 07:56:06 -0300

    Add files via upload

---
+++ TryAndCatch/TryAndCatch/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace TryAndCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            string userInput1 = Console.ReadLine();
+
+            Console.WriteLine("Now enter the number by which to divide");
+            string userInput2 = Console.ReadLine();
+
+            try
+            {
+                int userInputAsInt2 = int.Parse(userInput2);
+                int userInputAsInt1 = int.Parse(userInput1);
+                int result = userInputAsInt1 / userInputAsInt2;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Format exception, please enter the correct format.");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("DivideByZeroException, please enter a number other than 0");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Overflow exception, the number was too long or too short for an int32");
+            }
+            finally
+            {
+                Console.WriteLine("This is called anyways!");
+            }
+
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}

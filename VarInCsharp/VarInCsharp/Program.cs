90380acbbf6289b3beb72fcad340aa986ba38b70
RafaelMasetto <127772302+RafaelMasetto@users.noreply.github.com>
2024-01-15 07:57:04 -0300

    Add files via upload

---
+++ VarInCsharp/VarInCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace VarInCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 0; // int
+            var text = "Text"; // string
+            var isTrue = false; // boolean
+        }
+    }
+}

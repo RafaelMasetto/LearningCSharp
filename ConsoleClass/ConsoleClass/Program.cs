891fc0545b7b421c158b46108f694888e5cb5117
RafaelMasetto <127772302+RafaelMasetto@users.noreply.github.com>
2024-01-15 07:45:01 -0300

    Add files via upload

---
+++ ConsoleClass/ConsoleClass/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hi {0}, nice to meet you!", userName);
+
+            Console.Write("{0}, please, enter your age: ", userName);
+            int userAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}? Well {1}, it's the perfect age to start learning coding!", userAge, userName);
+
+        }
+    }
+}

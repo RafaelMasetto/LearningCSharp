370c589841e73121462d9c557751bf15b54aae0d
RafaelMasetto <127772302+RafaelMasetto@users.noreply.github.com>
2024-01-15 07:44:01 -0300

    Add files via upload

---
+++ ConstantsC/ConstantsC/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstantsC
+{
+    // Constants are imutable values which are known
+    //at compile time and do not change for the life of the program.
+    
+    class Program
+    {
+        // constants at fields
+        const double PI = 3.14159265359;
+        const int weeks = 52, months = 12;
+
+        const string myBirthday = "02.10.1991";
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hi there, my birthday will always be {myBirthday}.");
+            Console.ReadKey();
+            
+        }
+    }
+}

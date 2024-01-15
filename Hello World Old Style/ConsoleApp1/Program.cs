b3c2b09c2f9aa083fe684c42c5836f85c445cabe
RafaelMasetto <127772302+RafaelMasetto@users.noreply.github.com>
2024-01-15 07:34:10 -0300

    Add files via upload

---
+++ Hello World Old Style/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaring a variable
+            int num1;
+            // assigning a value to a variable
+            num1 = 13;
+
+            // declaring and initializing a variable in one go
+            int num2 = 23;
+            int sum = num1 + num2;
+
+            // using concatenation
+
+            Console.WriteLine("the sum of " + num1 + " and " + num2 + " is " + sum);
+
+            // declaring multiple variables at once
+            int num3, num4, num5;
+
+            double d1 = 3.1415;
+            double d2 = 5.1;
+            double dDiv = d1 / d2;
+            Console.WriteLine(d1 + " / " + d2 + " = " + dDiv);
+
+            float f1 = 3.1415f;
+            float f2 = 5.1f;
+            float fDiv = f1 / f2;
+            Console.WriteLine(f1 + " / " + f2  + " = " + fDiv);
+
+            // Datatype string
+
+            string myName = "Rafael Masetto";
+            Console.WriteLine("Hello there, my name is " + myName);
+
+            string greeting = "Hello there, my name is " + myName;
+            Console.WriteLine(greeting);
+
+            // using String methods
+            Console.WriteLine(greeting.ToUpper());
+
+
+            Console.Read();
+        }
+    }
+}

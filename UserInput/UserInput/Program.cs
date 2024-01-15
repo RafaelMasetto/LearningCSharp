00db585d3df2b1916255148bb9e01330d4013935
RafaelMasetto <127772302+RafaelMasetto@users.noreply.github.com>
2024-01-15 07:56:39 -0300

    Add files via upload

---
+++ UserInput/UserInput/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace UserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {;
+            Add();
+            
+            
+            Console.Read();
+        }
+
+        public static void Add()
+        {
+            Console.WriteLine("Please, enter the first number we're going to add: ");
+            string num1Input = Console.ReadLine();
+            Console.WriteLine("Now enter the second number: ");
+            string num2Input = Console.ReadLine();
+
+            int num1 = int.Parse(num1Input);
+            int num2 = int.Parse(num2Input);
+            
+            
+            int result = num1 + num2;
+
+            Console.WriteLine($"The total is {result}.");
+        }
+
+
+
+
+    }
+}

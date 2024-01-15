0fe64ebe8cc1e62d2792df15c3dc958c86a8ce53
RafaelMasetto <127772302+RafaelMasetto@users.noreply.github.com>
2024-01-15 07:42:08 -0300

    Add files via upload

---
+++ SpecialCharacterString/SpecialCharacterString/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+namespace SpecialCharacterString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "this is a \"string\" with \na backslash \\ and a colon: ";
+            Console.WriteLine(s1);
+
+        }
+    }
+}

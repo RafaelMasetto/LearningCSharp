87f2c0e599aa09bbdcff8f4f83a1cc15c3983da7
RafaelMasetto <127772302+RafaelMasetto@users.noreply.github.com>
2024-01-15 07:43:23 -0300

    Add files via upload

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteSomething();
+            WriteSomethingSpecific("I am an argument and I am called from a method");
+            Console.Read();        }
+
+        // access modifier (Static) return type method name (parameter1, parameter2)
+        public static void WriteSomething()
+        {
+            Console.WriteLine("I am called from a method");
+        }
+
+        public static void WriteSomethingSpecific(string myText)
+        {
+            Console.WriteLine(myText);
+        }
+    }
+}
